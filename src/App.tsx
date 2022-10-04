0d2d0bf31838888edd0e3e9e50794f029813f19c
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-03 21:29:35 -0400

    installed react-router and set up navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import CreateProject from "./screens/CreateProject/CreateProject";
 import "./App.css";
+import Landing from "./screens/Landing/Landing";
+import SignIn from "./screens/SignIn/SignIn";
+import SignUp from "./screens/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sign-up">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/">Landing Page</Link>
+            </li>
+            <li>
+              <Link to="/projects/create">Create Project</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/projects/create" element={<CreateProject />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
