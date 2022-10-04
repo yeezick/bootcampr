6d30134a858688a98dd5e088713c1217d092f51a
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-03 21:42:33 -0400

    Set up component folder with header + footer file

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateProject from "./screens/CreateProject/CreateProject";
 import "./App.css";
 import Landing from "./screens/Landing/Landing";
 import SignIn from "./screens/SignIn/SignIn";
 import SignUp from "./screens/SignUp/SignUp";
+import NavigationMenu from "./layout/NavigationMenu/NavigationMenu";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/sign-up">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/sign-in">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/">Landing Page</Link>
-            </li>
-            <li>
-              <Link to="/projects/create">Create Project</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/projects/create" element={<CreateProject />} />
-        </Routes>
-      </div>
+      <NavigationMenu />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/projects/create" element={<CreateProject />} />
+      </Routes>
     </Router>
   );
 }
