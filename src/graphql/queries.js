f1640ee2e36435d2481b09a1773f8109efb2f774
Erick Manrique <ramerick5@gmail.com>
2022-10-05 19:48:59 -0400

    create graphql api with expected user/project types and update App.tsx with template from tutorial

---
+++ src/graphql/queries.js
@@ -0,0 +1,122 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = /* GraphQL */ `
+  query GetNote($id: ID!) {
+    getNote(id: $id) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listNotes = /* GraphQL */ `
+  query ListNotes(
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      bio
+      description
+      email
+      name
+      profilePicture
+      role
+      supportingLinks
+      declinedProjects
+      interestedProjects
+      ownerOf
+      savedProjects
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        bio
+        description
+        email
+        name
+        profilePicture
+        role
+        supportingLinks
+        declinedProjects
+        interestedProjects
+        ownerOf
+        savedProjects
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getProject = /* GraphQL */ `
+  query GetProject($id: ID!) {
+    getProject(id: $id) {
+      id
+      duration
+      meetingCadence
+      members
+      openRoles
+      overview
+      owner
+      status
+      tools
+      title
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listProjects = /* GraphQL */ `
+  query ListProjects(
+    $filter: ModelProjectFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProjects(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        duration
+        meetingCadence
+        members
+        openRoles
+        overview
+        owner
+        status
+        tools
+        title
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
