76c7504432a37e384ba4fba38838d5329114bbd4
Erick Manrique <ramerick5@gmail.com>
2022-10-15 17:41:58 -0500

    resolve type errors on app.tsx, converts the file to TS and prevents app from breaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, FormEventHandler } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API } from "aws-amplify";
@@ -11,97 +11,105 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
+import { listProjects } from "./graphql/queries";
 import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
+  createProject as createProjectMutation,
+  deleteProject as deleteProjectMutation,
 } from "./graphql/mutations";
 
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
+type Props = {
+  signOut?: React.FunctionComponent
+};
+interface ProjectRefs {
+  projectID: string
+}
+interface Project {
+  id: number,
+  bio: string,
+  description: string,
+  email:string,
+  name: string,
+  profilePicture: string,
+  role: string, 
+  supportingLinks: string,
+  declinedProjects: ProjectRefs[],
+  interestedProjects: ProjectRefs[],
+  ownerOf: ProjectRefs[],
+  savedProjects: ProjectRefs[],
+}
+
+
+export const App = ({ signOut }: Props) => {
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    fetchNotes();
+    fetchProjects();
   }, []);
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    setNotes(notesFromAPI);
+  const fetchProjects = async () =>  {
+    const apiData: any = await API.graphql({ query: listProjects });
+    const projectsFromAPI = apiData.data.listProjects.items;
+    setProjects(projectsFromAPI);
   }
 
-  async function createNote(event) {
+  const createProject = async (event: React.SyntheticEvent<HTMLFormElement> ) => {
     event.preventDefault();
-    const form = new FormData(event.target);
+    const form = new FormData(event.currentTarget);
     const data = {
       name: form.get("name"),
       description: form.get("description"),
     };
     await API.graphql({
-      query: createNoteMutation,
+      query: createProjectMutation,
       variables: { input: data },
     });
-    fetchNotes();
-    event.target.reset();
+    fetchProjects();
+    event.currentTarget.reset();
   }
+  
 
-  async function deleteNote({ id }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
+  const deleteProject = async ({ id }: any) => {
+    const newProjects = projects.filter((project: Project) => project.id !== id);
+    setProjects(newProjects);
     await API.graphql({
-      query: deleteNoteMutation,
+      query: deleteProjectMutation,
       variables: { input: { id } },
     });
   }
-
+// replace AWS components with standard React
   return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
+    <div className="App">
+      <Heading level={1}>My Projects</Heading>
+      <h1>My Projects</h1>
+      <form onSubmit={createProject}>
+        <div style={{display: "flex", justifyContent:"center", margin:"3rem 0"}}>
+          {/* AWS had the labels hidden and instead defined by the placeholder */}
+          <label>Project Name</label>
+          <input type="text" placeholder="Project Name" required />
+
+          <label>Description</label>
+          <input type="text" placeholder="Project Description" required />
+
+          <button>Create Project</button>
+        </div>
+      </form>
+        <h2>View Projects</h2>
+        <div style={{margin: "3rem 0"}}>
+          {projects.map((project: Project, mapIdx) => (
+            <div style={{display: "flex", justifyContent:"center", alignItems: "center"}} key={mapIdx}>
             <Text as="strong" fontWeight={700}>
-              {note.name}
+              {project.name}
             </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
+            <Text as="span">{project.description}</Text>
+            <Button variation="link" onClick={() => deleteProject(project)}>
+              Delete project
             </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
-};
+            </div>
+          ))}
+        </div>
+        <Button onClick={signOut}>Sign Out</Button>
+    </div>
+  )
+}
 
 export default withAuthenticator(App);
