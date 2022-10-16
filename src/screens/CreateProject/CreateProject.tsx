0598d8cddeaa7d088b1d43c54553e458d1db0d2c
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-16 13:18:36 -0400

    add inputs to a form and file upload option

--- src/screens/CreateProject/CreateProject.tsx
+++ src/screens/CreateProject/CreateProject.tsx
@@ -7,57 +7,64 @@ const CreateProject: React.FC<CreateProjectProps> = (props) => {
     <div className="create-project">
       <p className="heading">Create a Project</p>
 
-      <label htmlFor="title">Title</label>
-      <input type="text" name="title" />
+      <form action="" className="project-form">
+        <label htmlFor="photo" className="photo">
+          Upload Image
+          <input type="file" />
+        </label>
 
-      <label htmlFor="project-owner">Project Owner</label>
-      <input type="text" name="project-owner" />
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" />
 
-      <label htmlFor="industry">Industry</label>
-      <select name="industry">
-        <optgroup label="Industry">
-          <option value="0"></option>
-          <option value="1">Retail</option>
-          <option value="2">Banking</option>
-          <option value="3">Healthcare</option>
-        </optgroup>
-      </select>
+        <label htmlFor="project-owner">Project Owner</label>
+        <input type="text" name="project-owner" />
 
-      <label htmlFor="roles">Roles</label>
-      <select name="roles">
-        <optgroup label="Roles">
-          <option value="0"></option>
-          <option value="1">Software Engineer</option>
-          <option value="2">UX Designer</option>
-          <option value="3">Scrum Master</option>
-        </optgroup>
-      </select>
+        <label htmlFor="industry">Industry</label>
+        <select name="industry">
+          <optgroup label="Industry">
+            <option value="0"></option>
+            <option value="1">Retail</option>
+            <option value="2">Banking</option>
+            <option value="3">Healthcare</option>
+          </optgroup>
+        </select>
 
-      <label htmlFor="technologies-used">Technologies Used</label>
-      <select name="technologies-used">
-        <optgroup label="Technologies">
-          <option value="0"></option>
-          <option value="1">React</option>
-          <option value="2">Redux</option>
-          <option value="3">Typescript</option>
-        </optgroup>
-      </select>
+        <label htmlFor="roles">Roles</label>
+        <select name="roles">
+          <optgroup label="Roles">
+            <option value="0"></option>
+            <option value="1">Software Engineer</option>
+            <option value="2">UX Designer</option>
+            <option value="3">Scrum Master</option>
+          </optgroup>
+        </select>
 
-      <label htmlFor="meeting-cadence">Meeting Cadence</label>
-      <select name="meeting-cadence">
-        <optgroup label="Meeting cadence">
-          <option value="0"></option>
-          <option value="1">Once per month</option>
-          <option value="2">Once per week</option>
-          <option value="3">Daily</option>
-        </optgroup>
-      </select>
+        <label htmlFor="technologies-used">Technologies Used</label>
+        <select name="technologies-used">
+          <optgroup label="Technologies">
+            <option value="0"></option>
+            <option value="1">React</option>
+            <option value="2">Redux</option>
+            <option value="3">Typescript</option>
+          </optgroup>
+        </select>
 
-      <label htmlFor="overview">Overview</label>
-      <textarea name="overview" className="overview" cols={30} rows={10}></textarea>
+        <label htmlFor="meeting-cadence">Meeting Cadence</label>
+        <select name="meeting-cadence">
+          <optgroup label="Meeting cadence">
+            <option value="0"></option>
+            <option value="1">Once per month</option>
+            <option value="2">Once per week</option>
+            <option value="3">Daily</option>
+          </optgroup>
+        </select>
 
-      <button>Save as Draft</button>
-      <button>Publish</button>
+        <label htmlFor="overview">Overview</label>
+        <textarea name="overview" className="overview" cols={30} rows={10}></textarea>
+
+        <button>Save as Draft</button>
+        <button>Publish</button>
+      </form>
     </div>
   );
 };
