9d82f04c59e6dffb107832739a6b61d843967887
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-16 13:17:04 -0400

    values for menu, examples only, none on figma

--- src/screens/CreateProject/CreateProject.tsx
+++ src/screens/CreateProject/CreateProject.tsx
@@ -14,16 +14,44 @@ const CreateProject: React.FC<CreateProjectProps> = (props) => {
       <input type="text" name="project-owner" />
 
       <label htmlFor="industry">Industry</label>
-      <select name="industry"></select>
+      <select name="industry">
+        <optgroup label="Industry">
+          <option value="0"></option>
+          <option value="1">Retail</option>
+          <option value="2">Banking</option>
+          <option value="3">Healthcare</option>
+        </optgroup>
+      </select>
 
       <label htmlFor="roles">Roles</label>
-      <select name="roles"></select>
+      <select name="roles">
+        <optgroup label="Roles">
+          <option value="0"></option>
+          <option value="1">Software Engineer</option>
+          <option value="2">UX Designer</option>
+          <option value="3">Scrum Master</option>
+        </optgroup>
+      </select>
 
       <label htmlFor="technologies-used">Technologies Used</label>
-      <select name="technologies-used"></select>
+      <select name="technologies-used">
+        <optgroup label="Technologies">
+          <option value="0"></option>
+          <option value="1">React</option>
+          <option value="2">Redux</option>
+          <option value="3">Typescript</option>
+        </optgroup>
+      </select>
 
       <label htmlFor="meeting-cadence">Meeting Cadence</label>
-      <select name="meeting-cadence"></select>
+      <select name="meeting-cadence">
+        <optgroup label="Meeting cadence">
+          <option value="0"></option>
+          <option value="1">Once per month</option>
+          <option value="2">Once per week</option>
+          <option value="3">Daily</option>
+        </optgroup>
+      </select>
 
       <label htmlFor="overview">Overview</label>
       <textarea name="overview" className="overview" cols={30} rows={10}></textarea>
