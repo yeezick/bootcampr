d3ee4a37e7300931a8e48708622eb6818e52d956
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-19 17:35:30 -0400

    add hook to fetch projects and display on screen

--- src/screens/Projects/Projects.tsx
+++ src/screens/Projects/Projects.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAllProjects } from '../../api/projects';
 
 const Projects = () => {
+  const [projects, setProjects] = useState<any>({});
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const displayAllProjects = await getAllProjects();
+      setProjects(displayAllProjects);
+    };
+    fetchProjects();
+  }, [setProjects]);
+
+  // console.log(projects);
+
   return (
     <div>
       <h1>Browse Projects</h1>
+      <ul>
+        {projects.map((project: any) => {
+          return (
+            <li>
+              <p>{project.title}</p>
+              <p>{project.overview}</p>
+              <p>{project.meeting_cadence}</p>
+              <p>{project.duration}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
