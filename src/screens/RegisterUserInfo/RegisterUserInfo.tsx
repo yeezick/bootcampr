e3bfac5785bc89aa6ff831bfae4323e0739d264f
Jason Martinez <jasonmar08@gmail.com>
2022-10-24 03:48:35 -0400

    create form template for RegisterUserInfo component

--- src/screens/RegisterUserInfo/RegisterUserInfo.tsx
+++ src/screens/RegisterUserInfo/RegisterUserInfo.tsx
@@ -4,7 +4,18 @@ const RegisterUserInfo: React.FC = () => {
   return (
     <div>
       <h3>Register User Info</h3>
-
+      <h2>Hi, (User's name goes here)</h2>
+      <form>
+        <input type='text' name='first_name' placeholder='First Name' value=''></input>
+        <input type='text' name='last_name' placeholder='Last Name' value=''></input>
+        <textarea name='about' placeholder='About Me' value='' ></textarea>
+        <input type='text' name='interested_projects' placeholder='Projects of Interest' value=''></input>
+        <input type='text' name='member_of_projects' placeholder='# Of Projects' value=''></input>
+        <input type='text' name='portfolio_link' placeholder='Portfolio URL' value=''></input>
+        <input type='text' name='portfolio_projects' placeholder='Portfolio Projects' value=''></input>
+        <input type='text' name='rejected_projects' placeholder='Rejected Projects' value=''></input>
+        <input type='text' name='role' placeholder='Role/Position' value=''></input>
+      </form>
     </div>
   )
 };
