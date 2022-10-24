3e959ff9f376f25713dff1dbe0da7e8ff859eff7
Jason Martinez <jasonmar08@gmail.com>
2022-10-24 03:55:06 -0400

    add Save Profile buttont to RegisterUserInfo component

--- src/screens/RegisterUserInfo/RegisterUserInfo.tsx
+++ src/screens/RegisterUserInfo/RegisterUserInfo.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const RegisterUserInfo: React.FC = () => {
+
+  const handleUpdateProfile = () => {}
+
   return (
     <div>
       <h3>Register User Info</h3>
       <h2>Hi, (User's name goes here)</h2>
-      <form>
+      <form onSubmit={handleUpdateProfile}>
         <input type='text' name='first_name' placeholder='First Name' value=''></input>
         <input type='text' name='last_name' placeholder='Last Name' value=''></input>
         <textarea name='about' placeholder='About Me' value='' ></textarea>
@@ -15,6 +19,7 @@ const RegisterUserInfo: React.FC = () => {
         <input type='text' name='portfolio_projects' placeholder='Portfolio Projects' value=''></input>
         <input type='text' name='rejected_projects' placeholder='Rejected Projects' value=''></input>
         <input type='text' name='role' placeholder='Role/Position' value=''></input>
+        <button>Save Profile</button>
       </form>
     </div>
   )
