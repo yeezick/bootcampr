7eb3f09f1f825088eb5d07a3038535dadf5d2bfe
Jason Martinez <jasonmar08@gmail.com>
2022-10-24 20:42:14 -0400

    create sign up form boilerplate with handleChange and handleSubmit functions

--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -3,17 +3,27 @@ import { SignUpInterface } from "../../utilities/Interface/SignUpInterface";
 
 const SignUp: React.FC = () => {
   const [signUp, setSignUp ] = useState<SignUpInterface>()
+
+  const handleChange= (e) =>{
+    
+  }
   
-  return( <>
-  <div> 
-    <form>
-      <input type="text" name="email" placeholder="Email" />
-      <input type="text" name="first_name" placeholder="First Name" />
-      <input type="text" name="last_name" placeholder="Last Name" />
-      <input type="text" name="password" placeholder="Password" />
-      <input type="text" name="confirmPassword" placeholder="Confirm Password"/>
-    </form>
-  </div>
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+  }
+
+  return ( 
+  <>
+    <div> 
+      <form >
+        <input type="text" name="email" placeholder="Email"  onChange={handleChange}/>
+        <input type="text" name="first_name" placeholder="First Name"    onChange={handleChange}/>
+        <input type="text" name="last_name" placeholder="Last Name"   onChange={handleChange}/>
+        <input type="text" name="password" placeholder="Password"  onChange={handleChange} />
+        <input type="text" name="confirmPassword" placeholder="Confirm Password"  onChange={handleChange}/>
+        <button type="submit"> sign up</button>
+      </form>
+    </div>
   </>)
 };
 
