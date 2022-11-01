e518e9a4fe2e361fbf27a4d6b8c208850e0b21c4
Jason Martinez <jasonmar08@gmail.com>
2022-11-01 01:36:53 -0400

    structure AdditionalInfo screen

--- src/screens/AdditionalInfo/AdditionalInfo.tsx
+++ src/screens/AdditionalInfo/AdditionalInfo.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useAppSelector } from "../../utilities/redux/hooks";
+import './AdditionalInfo.scss'
 
 const AdditionalInfo: React.FC = () => {
 
-  const { firstName, lastName } = useAppSelector((state) => state.ui.auth.user)
+  const { bio, email, firstName, lastName, linkedinUrl, portfolioUrl, profilePicture, role } = useAppSelector((state) => state.ui.auth.user)
 
   return (
-    <div>
-      <h2>Additional Info</h2>
-      <h3>{firstName}</h3>
-      <h3>{lastName}</h3>
+    <div className="acct-setup-container">
+      <img src='https://tinyurl.com/2tbvwnfb' alt='photo' />
+      <h2>Set Up Your Profile</h2>
+      <h3>First Name: {firstName}</h3>
+      <h3>Last Name: {lastName}</h3>
+      <h3>Email: {email}</h3>
+      <h3>Bio: {bio}</h3>
+      <h3>Role: {role}</h3>
+      <h2>Socials</h2>
+      <h3>LinkedIn URL: {linkedinUrl}</h3>
+      <h3>Portfolio URL: {portfolioUrl}</h3>
+      <h3>Profile Photo URL: {portfolioUrl}</h3>
     </div>
   )
 }
