630b3d726740491432be388367a7ccfff7eb1c42
Rina Mendoza <rinamendozaa@gmail.com>
2022-11-19 18:33:17 -0500

    projects now displaying on My Projects nav link due to populating it in the backend

--- src/screens/UserProjects/UserProjects.tsx
+++ src/screens/UserProjects/UserProjects.tsx
@@ -4,45 +4,29 @@ import { getOneUser } from '../../utilities/api/users';
 import { selectAuthUser } from '../../utilities/redux/slices/users/userSlice';
 import { useAppSelector } from '../../utilities/redux/hooks';
 import { ProjectInterface } from '../../utilities/types/ProjectInterface';
-import { emptyProject, emptyProjectOwner } from '../../utilities/data/projectConstants';
+import { emptyProject } from '../../utilities/data/projectConstants';
+import { UserInterface } from '../../utilities/types/UserInterface';
 
 export const UserProjects = () => {
   const authUser = useAppSelector(selectAuthUser);
   const { _id: userId, ownerOfProjects } = authUser;
-  const [projectOwnerDetails, setProjectOwnerDetails] = useState<ProjectInterface>(emptyProject);
-  const [projectOwner, setProjectOwner] = useState(emptyProjectOwner);
-
+  //   const [projectOwnerDetails, setProjectOwnerDetails] = useState<UserInterface>(emptyProject);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getProjectOwnerDetails = async () => {
-      const displayProjectOwnerDetails = await getOneUser(userId);
-      let projectOwner = await getOneUser(displayProjectOwnerDetails.project_owner);
-      setProjectOwner(projectOwner);
-      setProjectOwnerDetails(displayProjectOwnerDetails);
-    };
-    getProjectOwnerDetails();
-  }, [setProjectOwnerDetails]);
+  console.log(ownerOfProjects);
 
-  console.log(projectOwnerDetails.roles);
-  //   const {
-  //     createdAt,
-  //     duration,
-  //     meeting_cadence,
-  //     overview,
-  //     roles,
-  //     status,
-  //     technologies_used,
-  //     title,
-  //     _id: projectId,
-  //   } = projectOwnerDetails;
+  useEffect(() => {
+    const asdf = authUser.ownerOfProjects?.map((proj) => {
+      console.log(proj);
+    });
+  }, []);
 
   return (
     <>
       {ownerOfProjects?.map((project: any) => (
         <div key={project._id}>
           <h1>{project.title}</h1>
-          <Link to={`/projects/${project}/edit`}>Edit My Project</Link>
+          <Link to={`/projects/${project._id}/edit`}>Edit My Project</Link>
         </div>
       ))}
     </>
