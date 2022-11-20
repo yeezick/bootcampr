90307a86b05bf25156087f091a0cf59516141e64
Rina Mendoza <rinamendozaa@gmail.com>
2022-11-19 19:33:34 -0500

    create skeleton for how user's projects will be displayed

--- src/screens/UserProjects/UserProjects.tsx
+++ src/screens/UserProjects/UserProjects.tsx
@@ -10,13 +10,29 @@ export const UserProjects = () => {
   console.log(ownerOfProjects);
 
   return (
-    <>
+    <div className="projects-container">
       {ownerOfProjects?.map((project: any) => (
         <div key={project._id}>
           <h1>{project.title}</h1>
-          <Link to={`/projects/${project._id}/edit`}>Edit My Project</Link>
+          <div className="status">
+            <p>Status</p>
+            <p>{project.status}</p>
+            <button>
+              <Link to={`/projects/${project._id}/edit`}>Edit My Project</Link>
+            </button>
+          </div>
+          <div>
+            <button>PROJECT DETAILS</button>
+            <button>ROLES</button>
+            <button>MEETINGS</button>
+          </div>
+          <p className="subheading">Description</p>
+          <p>{project.overview}</p>
+          <p className="subheading">Industry</p>
+          <p className="subheading">Technologies Used</p>
+          <p>{project.technologies_used}</p>
         </div>
       ))}
-    </>
+    </div>
   );
 };
