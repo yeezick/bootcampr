764e675187d45b6a6aacf9ce8a8b6261065f0e1c
Erick Manrique <ramerick5@gmail.com>
2023-02-12 11:58:33 -0600

    test prettier hool

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Layout } from 'layout';
-import { Landing } from 'screens/Landing/Landing';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Layout} from 'layout'
+import {Landing} from 'screens/Landing/Landing'
 // import { ProjectDetails } from 'screens/Projects';
-import { RegisterUserInfo } from 'screens/Auth';
-import { SignIn } from 'screens/Auth';
-import { SignUp } from 'screens/Auth';
-import { UserProfile } from 'screens/UserProfile';
-import { EditProfile } from 'screens/UserProfile';
+import {RegisterUserInfo} from 'screens/Auth'
+import {SignIn} from 'screens/Auth'
+import {SignUp} from 'screens/Auth'
+import {UserProfile} from 'screens/UserProfile'
+import {EditProfile} from 'screens/UserProfile'
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/users/:id" element={<UserProfile />} />
-          <Route path="/users/:id/edit" element={<EditProfile />} />
-          <Route path="/users/:id/account-setup" element={<RegisterUserInfo />} />
+          <Route path='/' element={<Landing />} />
+          <Route path='/sign-up' element={<SignUp />} />
+                  <Route path='/sign-in' element={<SignIn />} />
+
+                      <Route path='/users/:id' element={<UserProfile />} />
+          <Route path='/users/:id/edit' element={<EditProfile />} />
+          <Route
+            path='/users/:id/account-setup'
+            element={<RegisterUserInfo />}
+          />
           {/* <Route path="/projects/:id" element={<ProjectDetails />} /> */}
         </Routes>
       </Layout>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
