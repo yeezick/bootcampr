5e54bffc02da9dc57f87d477b3b2fd1dbbed0872
Erick Manrique <ramerick5@gmail.com>
2023-02-25 20:50:48 -0600

    idk, too much, fts

--- src/components/Notifications/NotificationModal.tsx
+++ src/components/Notifications/NotificationModal.tsx
@@ -17,96 +17,6 @@ import { selectAuthUser } from 'utilities/redux/slices/userSlice'
 import './Notification.scss'
 import { NotificationInterface } from 'utilities/types/NotificationInterface'
 
-export interface SimpleDialogProps {
-  open: boolean
-  selectedValue: boolean
-  onClose: (value: boolean) => void
-  notifications: NotificationInterface[]
-  fetchNotifications: () => void
-}
-
-function SimpleDialog(props: SimpleDialogProps) {
-  const { onClose, selectedValue, open, notifications, fetchNotifications } =
-    props
-  const authUser = useAppSelector(selectAuthUser)
-
-  const handleClose = () => {
-    onClose(selectedValue)
-  }
-
-  const handleDeleteNotification = async (value: string) => {
-    const deleteOneNotification = await deleteNotification(value)
-    if (deleteOneNotification) fetchNotifications()
-  }
-
-  const handleReadNotification = async (value: Object) => {
-    const markOneNotificationAsRead = await markNotificationAsRead(value)
-    if (markOneNotificationAsRead) fetchNotifications()
-  }
-
-  const handleListItemClick = async (value: string) => {
-    if (value === 'Delete-All') {
-      await deleteAllNotifications(authUser._id)
-    }
-    if (value === 'Read-All') {
-      await markAllNotificationsAsRead(authUser._id)
-    }
-    fetchNotifications()
-  }
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>{authUser.firstName}'s Notifications</DialogTitle>
-      <button
-        className='delete-read-all'
-        onClick={() => handleListItemClick('Read-All')}
-      >
-        Mark All As Read
-      </button>
-      <List sx={{ pt: 0 }} className='notification-wrapper'>
-        {notifications.length !== 0 ? (
-          notifications.map((notification: NotificationInterface) => {
-            return (
-              <ListItem
-                key={notification._id}
-                className='notification-wrapper notifications'
-                id={notification.read ? 'read' : 'unread'}
-              >
-                {!notification.read && <span className='status'>unread</span>}
-                <h3>{notification.title}</h3>
-                <ListItemText>{notification.message}</ListItemText>
-                <div className='read-delete-btn'>
-                  <button
-                    className='delete-read-all'
-                    onClick={() => {
-                      handleReadNotification({ ...notification, read: true })
-                    }}
-                  >
-                    Mark Read
-                  </button>
-                  <button
-                    className='delete-read-all'
-                    onClick={() => {
-                      handleDeleteNotification(notification._id)
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </ListItem>
-            )
-          })
-        ) : (
-          <p>Notifications empty</p>
-        )}
-        <button onClick={() => handleListItemClick('Delete-All')}>
-          Delete All
-        </button>
-      </List>
-    </Dialog>
-  )
-}
-
 export const NotificationModal = () => {
   const authUser = useAppSelector(selectAuthUser)
   const [notifications, setNotifications] = useState<NotificationInterface[]>(
@@ -147,5 +57,94 @@ export const NotificationModal = () => {
         onClose={handleClose}
       />
     </div>
+  )
+}
+export interface SimpleDialogProps {
+  open: boolean
+  selectedValue: boolean
+  onClose: (value: boolean) => void
+  notifications: NotificationInterface[]
+  fetchNotifications: () => void
+}
+
+const SimpleDialog = (props: SimpleDialogProps) => {
+  const { onClose, selectedValue, open, notifications, fetchNotifications } =
+    props
+  const authUser = useAppSelector(selectAuthUser)
+
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
+
+  const handleDeleteNotification = async (value: string) => {
+    const deleteOneNotification = await deleteNotification(value)
+    if (deleteOneNotification) fetchNotifications()
+  }
+
+  const handleReadNotification = async (value: Object) => {
+    const markOneNotificationAsRead = await markNotificationAsRead(value)
+    if (markOneNotificationAsRead) fetchNotifications()
+  }
+
+  const handleListItemClick = async (value: string) => {
+    if (value === 'Delete-All') {
+      await deleteAllNotifications(authUser._id)
+    }
+    if (value === 'Read-All') {
+      await markAllNotificationsAsRead(authUser._id)
+    }
+    fetchNotifications()
+  }
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>{authUser.firstName}'s Notifications</DialogTitle>
+      <button
+        className='delete-read-all'
+        onClick={() => handleListItemClick('Read-All')}
+      >
+        Mark All As Read
+      </button>
+      <List sx={{ pt: 0 }} className='notification-wrapper'>
+        {notifications.length ? (
+          notifications.map((notification: NotificationInterface) => {
+            return (
+              <ListItem
+                key={notification._id}
+                className='notification-wrapper notifications'
+                id={notification.read ? 'read' : 'unread'}
+              >
+                {!notification.read && <span className='status'>unread</span>}
+                <h3>{notification.title}</h3>
+                <ListItemText>{notification.message}</ListItemText>
+                <div className='read-delete-btn'>
+                  <button
+                    className='delete-read-all'
+                    onClick={() => {
+                      handleReadNotification({ ...notification, read: true })
+                    }}
+                  >
+                    Mark Read
+                  </button>
+                  <button
+                    className='delete-read-all'
+                    onClick={() => {
+                      handleDeleteNotification(notification._id)
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </ListItem>
+            )
+          })
+        ) : (
+          <p>Notifications empty</p>
+        )}
+        <button onClick={() => handleListItemClick('Delete-All')}>
+          Delete All
+        </button>
+      </List>
+    </Dialog>
   )
 }
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import { server } from '../__tests__/mock-server/server'
+import { server } from './__tests__/mock-server/server'
 
 // Establish API mocking before all tests.
 beforeAll(() => server.listen())
