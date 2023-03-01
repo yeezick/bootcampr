cb806d395a320ef989caf9a933c9c38b0a4bdb2e
Erick Manrique <ramerick5@gmail.com>
2023-03-01 09:12:22 -0600

    add basic test to landing page

--- src/__tests__/screens/Landing.spec.tsx
+++ src/__tests__/screens/Landing.spec.tsx
@@ -2,14 +2,10 @@ import { render, screen } from '__tests__/custom-render'
 import { Landing } from 'screens/Landing/Landing'
 
 describe('Landing page', () => {
-  let initialState, initialProps
-  beforeEach(() => {
-    ;(initialState = {}), (initialProps = {})
-  })
-
   test('User can sign up from the landing page', () => {
-    render(<Landing />, initialState)
-    screen.debug()
-    expect(true).toBe(true)
+    render(<Landing />)
+    expect(
+      screen.getByText(/surpass your competition in the tech job market/i)
+    ).toBeInTheDocument()
   })
 })
