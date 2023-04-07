b01ebc4dcc6aafb86756d4bd660734abc2843e2a
gooliajulia929 <juliadwyer93@gmail.com>
2023-04-06 23:38:44 -0400

    initial availability UI styling and structure

--- src/components/Availability/Availability.tsx
+++ src/components/Availability/Availability.tsx
@@ -1,30 +1,59 @@
-// Move all of this logic to a components folder for Availability
-// and write this a just a screen to render for demo-ing
+import { useState } from 'react'
+import { Checkbox, MenuItem, Select } from '@mui/material'
+import { ExpandMoreRounded } from '@mui/icons-material'
+import './Availability.scss'
+
 enum Weekdays {
   sunday = 'SUN',
   monday = 'MON',
 }
 console.log(Weekdays.sunday)
 
-const weekdays = [
-  'sunday',
-  'monday',
-  'tuesday',
-  'wednesday',
-  'thursday',
-  'friday',
-  'saturday',
-]
+enum Timezones {
+  MST = 'Mountain Time - US & Canada',
+}
+
+const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
 export const Availability: React.FC = (): JSX.Element => {
+  const [timezone, setTimezone] = useState(Timezones.MST)
   // STATE:
   // - each day:
   return (
     <div className='availability-container'>
-      <TimeZoneInput />
+      <TimeZoneInputBanner timezone={timezone} setTimezone={setTimezone} />
+      <p>Set weekly availability</p>
+      <hr />
       {weekdays.map(day => (
-        <DayAvailabilityInputBanner />
+        <DayAvailabilityInputBanner day={day} />
       ))}
+    </div>
+  )
+}
+
+const TimeZoneInputBanner = ({ setTimezone, timezone }) => {
+  return (
+    <div className='timezone-input-container'>
+      <h2>Time zone</h2>
+      {/* TODO: define this select component elsewhere? */}
+      <Select
+        defaultValue={timezone}
+        disableUnderline
+        IconComponent={ExpandMoreRounded}
+        sx={{
+          color: '#022888',
+          fontSize: '12px',
+          '& .MuiSvgIcon-root': {
+            color: '#022888',
+          },
+        }}
+        value={timezone}
+        variant='standard'
+        onChange={e => setTimezone(e.target.value)}
+      >
+        <MenuItem value={Timezones.MST}>Mountain Time - US & Canada</MenuItem>
+      </Select>
+      <TimeZoneInput />
     </div>
   )
 }
@@ -33,12 +62,24 @@ const TimeZoneInput = () => {
   return <div></div>
 }
 
-const DayAvailabilityInputBanner = () => {
+const DayAvailabilityInputBanner = ({ day }) => {
   return (
     <div>
-      {/* //checkbox // DAY // Unavailable OR... */}
-      <TimeSlotInput />
-      {/* // Add Timeslot Icon // Copy Availability Icon */}
+      <div className='day-availability-input-banner'>
+        <Checkbox
+          sx={{
+            color: '#022888',
+            '&.Mui-checked': {
+              color: '#022888',
+            },
+          }}
+        />
+        <h2>{day}</h2>
+        {/* //checkbox // DAY // Unavailable OR... */}
+        <TimeSlotInput />
+        {/* // Add Timeslot Icon // Copy Availability Icon */}
+      </div>
+      <hr />
     </div>
   )
 }
@@ -47,7 +88,8 @@ const TimeSlotInput = () => {
   return (
     <div>
       {/* // Start time input // - // End time input // Delete Icon */}
-      <SingleHourDropdownInput /> - <SingleHourDropdownInput />
+      <SingleHourDropdownInput />
+      <SingleHourDropdownInput />
     </div>
   )
 }
