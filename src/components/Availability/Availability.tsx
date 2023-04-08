6def22705029bd7d5161f42de46947aebff08a21
gooliajulia929 <juliadwyer93@gmail.com>
2023-04-07 21:55:39 -0400

    limit input options and delete slot functionality

--- src/components/Availability/Availability.tsx
+++ src/components/Availability/Availability.tsx
@@ -11,7 +11,7 @@ import {
   defaultAvailability,
   Timezones,
   timeOptions,
-  weekdays,
+  // weekdays,
 } from './utils/data'
 
 export const Availability: React.FC = (): JSX.Element => {
@@ -22,9 +22,13 @@ export const Availability: React.FC = (): JSX.Element => {
       <TimeZoneInputBanner timezone={timezone} setTimezone={setTimezone} />
       <p>Set weekly availability</p>
       <hr />
-      {weekdays.map(day => (
-        <DayAvailabilityInputBanner day={day} />
-      ))}
+      <DayAvailabilityInputBanner day='SUN' />
+      <DayAvailabilityInputBanner day='MON' />
+      <DayAvailabilityInputBanner day='TUE' />
+      <DayAvailabilityInputBanner day='WED' />
+      <DayAvailabilityInputBanner day='THU' />
+      <DayAvailabilityInputBanner day='FRI' />
+      <DayAvailabilityInputBanner day='SAT' />
     </div>
   )
 }
@@ -55,16 +59,41 @@ const TimeZoneInputBanner = ({ setTimezone, timezone }) => {
   )
 }
 
+const getNextTimeslotRange = availability => {
+  const lastAvailableTime = availability[availability.length - 1][1]
+  const index = timeOptions.indexOf(lastAvailableTime)
+  const start = timeOptions[index + 1]
+  const end = timeOptions[index + 2]
+  if (['11:00 PM', '11:30 PM'].includes(lastAvailableTime)) {
+    return [timeOptions[0], timeOptions[1]]
+  } else {
+    return [start, end]
+  }
+}
+
 const DayAvailabilityInputBanner = ({ day }) => {
   const [days, setDays] = useState(defaultAvailability)
 
-  const handleChange = e => {
-    // consolidate availability function before storing
+  const handleCheck = e => {
     setDays({
       ...days,
       [e.target.name]: {
         available: !days[e.target.name].available,
-        availability: [...days[e.target.name].availability, ['test']],
+        availability: [...days[e.target.name].availability],
+      },
+    })
+  }
+
+  const handleAddSlot = (e, day) => {
+    const availability = days[day].availability
+    const nextSlot = getNextTimeslotRange(availability)
+    console.log(nextSlot)
+
+    setDays({
+      ...days,
+      [day]: {
+        availability: [...days[day].availability, nextSlot],
+        available: days[day].available,
       },
     })
   }
@@ -75,18 +104,23 @@ const DayAvailabilityInputBanner = ({ day }) => {
         <div className='left-banner'>
           <Checkbox
             name={day}
-            onChange={e => handleChange(e)}
+            onChange={e => handleCheck(e)}
             sx={{ color: '#022888', '&.Mui-checked': { color: '#022888' } }}
           />
           <h2>{day}</h2>
           {days[day]['available'] ? (
-            <TimeSlotInput day={day} />
+            <TimeSlotInput
+              day={day}
+              days={days}
+              setDays={setDays}
+              slots={days[day].availability}
+            />
           ) : (
             <h2>Unavailable</h2>
           )}
         </div>
         <div className='right-banner'>
-          <AddRounded />
+          <AddRounded onClick={e => handleAddSlot(e, day)} />
           <ContentCopyOutlined />
         </div>
       </div>
@@ -95,47 +129,99 @@ const DayAvailabilityInputBanner = ({ day }) => {
   )
 }
 
-const TimeSlotInput = ({ day }) => {
+// const consolidateAvailability = (availability: string[][], eventTarget) => {
+// }
+
+const subOptions = (startTime, isStart) => {
+  const index = timeOptions.indexOf(startTime)
+  return isStart ? timeOptions.slice(index) : timeOptions.slice(index + 1)
+}
+
+const TimeSlotInput = ({ day, days, setDays, slots }) => {
+  const handleTimeChange = e => {
+    // update availability arrays for day
+    // trigger a
+    const context = e.target.name.split('-')
+    const day = context[0]
+    const frame = Number(context[1])
+    const index = context[2] === 'start' ? 0 : 1
+    const oldAvailability = days[day].availability
+    const newAvailability = oldAvailability
+    newAvailability[frame][index] = e.target.value
+    setDays({
+      ...days,
+      [day]: {
+        available: days[day].available,
+        availability: newAvailability,
+      },
+    })
+    console.log(days)
+  }
+
+  const handleDelete = (day, idx) => {
+    const oldAvailability = days[day].availability
+    const newAvailalability = oldAvailability
+    newAvailalability.splice(idx, 1)
+    setDays({
+      ...days,
+      [day]: {
+        available: days[day].available,
+        availability: newAvailalability,
+      },
+    })
+  }
+
   return (
-    <div className='timeslot-input'>
-      {/* Make a new custom component for these that takes in arguments like day, start/end, to keep this clean */}
-      <Select
-        name={`${day}-start`}
-        onChange={e => console.log(e.target.name)}
-        size='small'
-        defaultValue='9:00 AM'
-        inputProps={{ sx: { padding: '8px 13px !important' } }}
-        sx={{
-          padding: '0 !important',
-          fontSize: '14px',
-          '& .MuiSvgIcon-root': { display: 'none' },
-          backgroundColor: '#fefefe',
-          '.MuiOutlinedInput-notchedOutline': { border: 0 },
-        }}
-      >
-        {timeOptions.map(time => (
-          <MenuItem value={time}>{time}</MenuItem>
-        ))}
-      </Select>
-      <p>--</p>
-      <Select
-        name={`${day},end`}
-        onChange={e => console.log(e.target.name)}
-        size='small'
-        defaultValue='5:00 PM'
-        inputProps={{ sx: { padding: '8px 13px !important' } }}
-        sx={{
-          fontSize: '14px',
-          '& .MuiSvgIcon-root': { display: 'none' },
-          backgroundColor: '#fefefe',
-          '.MuiOutlinedInput-notchedOutline': { border: 0 },
-        }}
-      >
-        {timeOptions.map(time => (
-          <MenuItem value={time}>{time}</MenuItem>
-        ))}
-      </Select>
-      <DeleteOutline className='icon' />
+    <div>
+      {slots.map((slot, idx) => (
+        <div className='timeslot-input'>
+          {/* Make a new custom component for these that takes in arguments like day, start/end, to keep this clean */}
+          <Select
+            name={`${day}-${idx}-start`}
+            onChange={e => handleTimeChange(e)}
+            size='small'
+            defaultValue={slot[0]}
+            value={days[day].availability[idx][0]}
+            inputProps={{ sx: { padding: '8px 13px !important' } }}
+            sx={{
+              padding: '0 !important',
+              fontSize: '14px',
+              '& .MuiSvgIcon-root': { display: 'none' },
+              backgroundColor: '#fefefe',
+              '.MuiOutlinedInput-notchedOutline': { border: 0 },
+              width: '87px',
+            }}
+          >
+            {subOptions(slot[0], true).map(time => (
+              <MenuItem value={time}>{time}</MenuItem>
+            ))}
+          </Select>
+          <p>--</p>
+          <Select
+            name={`${day}-${idx}-end`}
+            onChange={e => handleTimeChange(e)}
+            size='small'
+            defaultValue={days[day].availability[idx][1]}
+            value={days[day].availability[idx][1]}
+            inputProps={{ sx: { padding: '8px 13px !important' } }}
+            sx={{
+              fontSize: '14px',
+              '& .MuiSvgIcon-root': { display: 'none' },
+              backgroundColor: '#fefefe',
+              '.MuiOutlinedInput-notchedOutline': { border: 0 },
+              width: '87px',
+            }}
+          >
+            {subOptions(slot[0], false).map(time => (
+              <MenuItem value={time}>{time}</MenuItem>
+            ))}
+          </Select>
+          <DeleteOutline
+            className='icon'
+            onClick={() => handleDelete(day, idx)}
+          />
+        </div>
+      ))}
     </div>
   )
 }
