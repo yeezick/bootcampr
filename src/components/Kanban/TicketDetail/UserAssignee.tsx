b3c88ce89272e351244edd969d576d31e502f7c4
Bestnogm1 <koffiarielhessou@gmail.com>
2023-05-31 11:30:04 -0400

    added a fake image for this use case until i get a real image]

--- src/components/Kanban/TicketDetail/UserAssignee.tsx
+++ src/components/Kanban/TicketDetail/UserAssignee.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import '../Ticket.scss'
+import { Icon } from '@mui/material'
 
-const UserAssignee = ({
-  user,
-  text,
-  detailIcon,
-  userImage,
-  UsersTitle,
-}: any) => {
+const UserAssignee = ({ userName, text, detailIcon, userImage, userRole }) => {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className='UserAssignee'>
+      <div className='UserAssignee-icon-text'>
+        <Icon>{detailIcon}</Icon>
+        <h1>{text}</h1>
+      </div>
+      <div className='UserAssignee-user-info'>
+        <div>
+          <img
+            src='https://issuetrackerimage.s3.amazonaws.com/0535deeb-4388-4bfd-b191-4c69d1e81cbc'
+            alt=''
+          />
+        </div>
+        <div>
+          <p>{userName}</p>
+          <p>{userRole}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
