583eeb28d78448d2d62be0464791d8cd1015bcd8
Erick Manrique <ramerick5@gmail.com>
2023-06-17 00:39:36 -0500

    create selector for user id create acountDropdown-skeleton and settings-raw screen

--- src/utils/redux/slices/userSlice.tsx
+++ src/utils/redux/slices/userSlice.tsx
@@ -151,6 +151,7 @@ const userSlice = createSlice({
 })
 
 export const selectAuthUser = (state: RootState) => state.ui.auth.user
+export const selectUserId = (state: RootState) => state.ui.auth.user._id
 export const getUserAvailability = (state: RootState) =>
   state.ui.auth.user.availability
 export const uiStatus = (state: RootState) => state.ui.status
