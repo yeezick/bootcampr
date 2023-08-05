2dbe009ad0f1383414c1b8c178b8b7ed841c9d53
Erick Manrique <ramerick5@gmail.com>
2023-08-05 17:03:18 -0500

    style time select dropdown to limit height limit available options for end time to be any option after the start time

--- src/components/Calendar/MeetingModal/SelectTime.tsx
+++ src/components/Calendar/MeetingModal/SelectTime.tsx
@@ -1,5 +1,5 @@
 import { MenuItem, Select } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { timeOptions } from 'utils/data/calendarConstants'
 import { combineDateWithTime, roundToNearestHalfHour } from 'utils/helpers'
 
@@ -7,7 +7,26 @@ export const SelectTime = ({ dateFields, dayjs, setDateFields, type }) => {
   const [selectedTime, setSelectedTime] = useState(
     roundToNearestHalfHour(dayjs(dateFields.date).format('hh:mm A'))
   )
+  const [availableOptions, setAvailableOptions] = useState(timeOptions)
   const { date } = dateFields
+
+  // update available options for end type to be next option after start time
+  useEffect(() => {
+    const currStartTime = roundToNearestHalfHour(
+      dateFields.start.format('h:mm A')
+    )
+    const currEndTime = roundToNearestHalfHour(dateFields.end.format('h:mm A'))
+    const endIdx = timeOptions.findIndex(option => option === currEndTime)
+    const startIdx = timeOptions.findIndex(option => option === currStartTime)
+    if (type === 'end') {
+      const filteredOptions = timeOptions.slice(startIdx + 1)
+      const isEndEarlierThanStart = startIdx > endIdx || startIdx === endIdx
+      setAvailableOptions(timeOptions.slice(startIdx + 1))
+      if (isEndEarlierThanStart) {
+        setSelectedTime(filteredOptions[0])
+      }
+    }
+  }, [dateFields.start])
 
   const handleTimeChange = e => {
     const { value } = e.target
@@ -21,6 +40,7 @@ export const SelectTime = ({ dateFields, dayjs, setDateFields, type }) => {
   return (
     <Select
       onChange={handleTimeChange}
+      MenuProps={{ sx: { marginTop: '3px', height: '400px' } }}
       size='small'
       sx={{
         background: '#ECEBEB',
@@ -28,10 +48,13 @@ export const SelectTime = ({ dateFields, dayjs, setDateFields, type }) => {
         '& .MuiOutlinedInput-notchedOutline': {
           border: 'none',
         },
+        '& .Mui-Paper-root': {
+          height: '50px',
+        },
       }}
       value={selectedTime}
     >
-      {timeOptions.map(time => (
+      {availableOptions.map(time => (
         <MenuItem key={`${type}-option-${time}`} value={time}>
           {time}
         </MenuItem>
