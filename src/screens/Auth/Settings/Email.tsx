2387715f2f9bc70c19dc3fd674e4933860c180a9
GooliaJulia <juliadwyer93@gmail.com>
2023-08-09 08:09:53 -0400

    include error and valid state message and logic

--- src/screens/Auth/Settings/Email.tsx
+++ src/screens/Auth/Settings/Email.tsx
@@ -9,10 +9,24 @@ export default function Email() {
   const [currentEmail, setCurrentEmail] = useState('testemail@mail.com')
   const [newEmail, setNewEmail] = useState('')
   const [reEnterNewEmail, setReEnterNewEmail] = useState('')
+  const [emailMatch, setEmailMatch] = useState(false)
+  const [nonEmpty, setNonEmpty] = useState(false)
+
+  const refreshForm = () => {
+    setNewEmail('')
+    setReEnterNewEmail('')
+  }
+
+  const checkIfEmailsMatch = () => {
+    const validation = newEmail === reEnterNewEmail
+    setEmailMatch(validation)
+  }
 
   useEffect(() => {
+    setNonEmpty(newEmail.length > 1 && reEnterNewEmail.length > 1)
     console.log(newEmail)
     console.log(reEnterNewEmail)
+    checkIfEmailsMatch()
   }, [newEmail, reEnterNewEmail])
 
   return (
@@ -31,8 +45,16 @@ export default function Email() {
         value={reEnterNewEmail}
         onChange={e => setReEnterNewEmail(e.target.value)}
       />
+      {nonEmpty &&
+        (emailMatch ? (
+          <p className='valid'>Email addresses match</p>
+        ) : (
+          <p className='invalid'>Email addresses do not match</p>
+        ))}
       <div className='buttons'>
-        <button className='cancel'>Cancel</button>
+        <button className='cancel' onClick={refreshForm}>
+          Cancel
+        </button>
         <button className='update'>Update email address</button>
       </div>
     </div>
