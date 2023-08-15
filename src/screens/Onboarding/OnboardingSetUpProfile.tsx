c1679ec9344882a3587b2c30e99bab788be63cdd
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2023-08-14 23:49:57 -0400

    added in new logic and styling

--- src/screens/Onboarding/OnboardingSetUpProfile.tsx
+++ src/screens/Onboarding/OnboardingSetUpProfile.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { selectAuthUser, setAuthUser } from 'utils/redux/slices/userSlice'
 import { emptyUser } from 'utils/data/userConstants'
@@ -10,7 +10,12 @@ import { useNotification } from 'utils/redux/slices/notificationSlice'
 import TextareaAutosize from 'react-textarea-autosize'
 import { IconButton } from '@mui/material'
 import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined'
+import { FiArrowRight, FiArrowLeft } from 'react-icons/fi'
 import './Onboarding.scss'
+
+// TODO: Alter functionality to be setup profile and not edit profile
+// TODO: Find out what and where Skip profile takes u and does
+// TODO: Confirm if Avatar will have initials or image upload as default here
 
 export const OnboardingSetUpProfile = () => {
   const authUser = useSelector(selectAuthUser)
@@ -90,15 +95,20 @@ export const OnboardingSetUpProfile = () => {
     return <div>Loading user...</div>
   }
 
+  let inputString =
+    "I'm from... I live in... I choose this career path because... my hobbies are... A fun fact about me is..."
+  let placeholder = inputString.replace(/\.\.\. /g, '...\n')
+
   return (
     <div className='onboarding'>
-      <div className='onboarding__profile-backContainer'></div>
-      <p className='onboarding__profile-heading'>Profile</p>
-      <p>
-        We recommend you set up your profile now. This will help your team get
-        to know you. You can start it, save it, and finish it later if you’d
-        like. You can edit your profile by going to My Account.
-      </p>
+      <div className='onboarding__profile-header-cont'>
+        <h2>Profile</h2>
+        <p>
+          We recommend you set up your profile now. This will help your team get
+          to know you. You can start it, save it, and finish it later if you’d
+          like. You can edit your profile by going to My Account.
+        </p>
+      </div>
       <div className='onboarding__profile-container'>
         <form onSubmit={handleUserUpdate} className='onboarding__profile-form'>
           <div className='onboarding__profile-image'>
@@ -140,6 +150,7 @@ export const OnboardingSetUpProfile = () => {
               value={bio}
               onChange={handleInputChange}
               maxLength={500}
+              placeholder={placeholder}
             />
             <div className='onboarding__profile-bioCharCount'>
               {bioCharCount}/500 characters
@@ -186,16 +197,21 @@ export const OnboardingSetUpProfile = () => {
               className='onboarding__profile-cancelBtn'
               onClick={handleCancel}
             >
-              Cancel
+              <FiArrowLeft className='onboarding__profile-arrow-l' />
+              <p>Availability</p>
             </button>
             <button
               type='submit'
               className='onboarding__profile-saveBtn'
               onClick={handleProfileSave}
             >
-              Save Profile
+              <p>Save profile</p>
+              <FiArrowRight className='onboarding__profile-arrow-r' />
             </button>
           </div>
+          <Link className='onboarding__profile-link' to=''>
+            Skip profile set up
+          </Link>
         </form>
       </div>
     </div>
