a982534e10e7a61ccf342db3fdc7f9109f08720e
laurenhatchett <laurenhatchett@gmail.com>
2023-09-27 17:30:41 -0400

    add TeamWithdrawal File and update UserProfile functionality to view user profile

--- src/screens/UserProfile/UserProfile.tsx
+++ src/screens/UserProfile/UserProfile.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { selectAuthUser } from 'utils/redux/slices/userSlice'
 import { IconButton } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
@@ -8,39 +8,36 @@ import { BsLink45Deg } from 'react-icons/bs'
 import { RiGithubLine } from 'react-icons/ri'
 import { FiLinkedin } from 'react-icons/fi'
 import './UserProfile.scss'
+import { UserInterface } from 'interfaces'
+import { useState, useEffect } from 'react'
+import { getOneUser } from 'utils/api'
 
 export const UserProfile: React.FC = () => {
+  const profileId = useParams()
+  const [userProfileInfo, setUser] = useState<any>()
   const authUser = useSelector(selectAuthUser)
   const navigate = useNavigate()
+  useEffect(() => {
+    async function getUser() {
+      console.log(profileId?.id)
+      const response = await getOneUser(profileId?.id)
+      const data = await response
+      console.log(data)
+      setUser(data)
+    }
+    getUser()
+  }, [])
 
   // BC-334: should handle this case
-  if (!authUser) {
-    return <div>Loading user... or there isn't one.</div>
-  }
-
-  const shouldShowName = authUser && authUser.firstName && authUser.lastName
-
-  const shouldShowRole =
-    authUser &&
-    (authUser.role === 'Software Engineer' || authUser.role === 'UX Designer')
-
-  const shouldShowBio = shouldShowRole && authUser && authUser.bio
-
-  const shouldShowPortfolioUrl =
-    shouldShowRole && authUser && authUser.links && authUser.links.portfolioUrl
-
-  const shouldShowGithubUrl =
-    authUser &&
-    authUser.role === 'Software Engineer' &&
-    authUser.links &&
-    authUser.links.githubUrl
-
-  const shouldShowLinkedInUrl =
-    shouldShowRole && authUser && authUser.links && authUser.links.linkedinUrl
+  // if (!userProfileInfo) {
+  //   return <div>Loading user... or there isn't one.</div>
+  // }
 
   const routeToEdit = () => {
     navigate(`/users/${authUser._id}/edit`)
   }
+
+  const sameProfile = () => (profileId?.id === authUser?._id ? true : false)
 
   return (
     <div className='userProfile'>
@@ -66,20 +63,32 @@ export const UserProfile: React.FC = () => {
             </IconButton>
           </div>
           <div className='userProfile__title'>
-            {shouldShowName && (
+            {userProfileInfo?.firstName && userProfileInfo?.lastName && (
               <h2>
-                {authUser.firstName} {authUser.lastName}
+                {userProfileInfo?.firstName} {userProfileInfo?.lastName}
               </h2>
             )}
-            {shouldShowRole && <h3>{authUser.role}</h3>}
+            {(userProfileInfo?.role === 'Software Engineer' ||
+              userProfileInfo?.role === 'UX Designer') && (
+              <h3>{userProfileInfo?.role}</h3>
+            )}
           </div>
-          <button className='userProfile__editBtn' onClick={routeToEdit}>
-            Edit Profile
-          </button>
+          {sameProfile() ? (
+            <button className='userProfile__editBtn' onClick={routeToEdit}>
+              Edit Profile
+            </button>
+          ) : (
+            <button className='userProfile__editBtn' onClick={routeToEdit}>
+              Send Message
+            </button>
+          )}
         </div>
         <div className='userProfile__infoContainer'>
           <h3>About me</h3>
-          {shouldShowBio && <p>{authUser.bio}</p>}
+          {(userProfileInfo?.role === 'Software Engineer' ||
+            userProfileInfo?.role === 'UX Designer') && (
+            <p>{userProfileInfo.bio}</p>
+          )}
         </div>
 
         <div className='userProfile__linksContainer'>
@@ -87,50 +96,56 @@ export const UserProfile: React.FC = () => {
             <BsLink45Deg className='userProfile__icons' />
             <div className='userProfile__link'>
               <h3>Portfolio</h3>
-              {shouldShowPortfolioUrl && (
-                <a
-                  className='userProfile__url'
-                  href={authUser.links.portfolioUrl}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {authUser.links.portfolioUrl}
-                </a>
-              )}
+              {(userProfileInfo?.role === 'Software Engineer' ||
+                userProfileInfo?.role === 'UX Designer') &&
+                userProfileInfo?.links &&
+                userProfileInfo?.links?.portfolioUrl && (
+                  <a
+                    className='userProfile__url'
+                    href={userProfileInfo?.links.portfolioUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {userProfileInfo?.links?.portfolioUrl}
+                  </a>
+                )}
             </div>
           </div>
-
           <div className='userProfile__linkItem'>
             <RiGithubLine className='userProfile__icons' />
             <div className='userProfile__link'>
               <h3>Github</h3>
-              {shouldShowGithubUrl && (
-                <a
-                  className='userProfile__url'
-                  href={authUser.links.githubUrl}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {authUser.links.githubUrl}
-                </a>
-              )}
+              {userProfileInfo?.role === 'Software Engineer' &&
+                userProfileInfo?.links &&
+                userProfileInfo?.links.githubUrl && (
+                  <a
+                    className='userProfile__url'
+                    href={userProfileInfo?.links.githubUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {userProfileInfo?.links.githubUrl}
+                  </a>
+                )}
             </div>
           </div>
-
           <div className='userProfile__linkItem'>
             <FiLinkedin className='userProfile__icons' />
             <div className='userProfile__linkLast'>
               <h3>LinkedIn</h3>
-              {shouldShowLinkedInUrl && (
-                <a
-                  className='userProfile__url'
-                  href={authUser.links.linkedinUrl}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {authUser.links.linkedinUrl}
-                </a>
-              )}
+              {(userProfileInfo?.role === 'Software Engineer' ||
+                userProfileInfo?.role === 'UX Designer') &&
+                userProfileInfo?.links &&
+                userProfileInfo?.links.linkedinUrl && (
+                  <a
+                    className='userProfile__url'
+                    href={userProfileInfo?.links.linkedinUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {userProfileInfo?.links.linkedinUrl}
+                  </a>
+                )}
             </div>
           </div>
         </div>
