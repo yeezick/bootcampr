a0dca320d0209485ffca98c0cd4ddbb6cff67460
GooliaJulia <juliadwyer93@gmail.com>
2023-11-08 18:00:00 -0700

    add email preferences api utils

---
+++ src/utils/api/communications.js
@@ -0,0 +1,22 @@
+import { api } from './apiConfig'
+
+export const getEmailPreferenceOptions = async () => {
+  try {
+    const res = await api.get('/bootcampr/emailPreferenceOptions')
+    return res.data
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+export const getUserEmailPreferences = async userId => {
+  try {
+    const res = await api.get(`/users/${userId}/emailPreferences`)
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
