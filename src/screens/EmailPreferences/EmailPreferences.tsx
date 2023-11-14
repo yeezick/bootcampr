a6aeea1ef5577233e60999e15b5d032075748898
GooliaJulia <juliadwyer93@gmail.com>
2023-11-14 09:04:09 -0700

    move email preference options and copy to frontend

--- src/screens/EmailPreferences/EmailPreferences.tsx
+++ src/screens/EmailPreferences/EmailPreferences.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react'
-import {
-  getEmailPreferenceOptions,
-  getUserEmailPreferences,
-} from '../../utils/api/communications.js'
+import { getUserEmailPreferences } from '../../utils/api/communications.js'
 import './EmailPreferences.scss'
 import { Checkbox, Switch } from '@mui/material'
 import { PrimaryButton } from 'components/Buttons'
 import { useAppSelector } from 'utils/redux/hooks'
 import { selectUserId } from 'utils/redux/slices/userSlice'
+import { emailPreferenceOptions } from 'utils/data/emailPreferenceConstants'
 
 export default function EmailPreferences() {
-  const [emailPreferenceOptions, setEmailPreferenceOptions] = useState({})
   const [disableAll, toggleDisableAll] = useState(false)
   const [userPreferences, setUserPreferences] = useState({
     bootcamprUpdates: true,
@@ -24,12 +21,6 @@ export default function EmailPreferences() {
   const userId = useAppSelector(selectUserId)
 
   useEffect(() => {
-    const getOptions = async () => {
-      const options = await getEmailPreferenceOptions()
-      setEmailPreferenceOptions(options)
-    }
-    getOptions()
-
     const getUserPreferences = async () => {
       const preferences = await getUserEmailPreferences(userId)
       setUserPreferences(preferences)
@@ -56,27 +47,26 @@ export default function EmailPreferences() {
           Select the types of email you would like to receive from Bootcampr.
         </h3>
         <div className='email-preference-options'>
-          {Object.keys(emailPreferenceOptions).length &&
-            Object.keys(emailPreferenceOptions).map(option => (
-              // TODO: Make component for each option
-              <div className='email-preference-option' key={option}>
-                <Checkbox
-                  name={option}
-                  checked={userPreferences[option]}
-                  disabled={disableAll}
-                  onChange={e => handleChecks(e, option)}
-                  className='email-preference-option-checkbox'
-                />
-                <div
-                  className={`email-preference-option-text ${
-                    disableAll && 'disabled'
-                  }`}
-                >
-                  <h4>{emailPreferenceOptions[option]?.title}</h4>
-                  <p>{emailPreferenceOptions[option]?.description}</p>
-                </div>
+          {Object.keys(emailPreferenceOptions).map(option => (
+            // TODO: Make component for each option
+            <div className='email-preference-option' key={option}>
+              <Checkbox
+                name={option}
+                checked={userPreferences[option]}
+                disabled={disableAll}
+                onChange={e => handleChecks(e, option)}
+                className='email-preference-option-checkbox'
+              />
+              <div
+                className={`email-preference-option-text ${
+                  disableAll && 'disabled'
+                }`}
+              >
+                <h4>{emailPreferenceOptions[option]?.title}</h4>
+                <p>{emailPreferenceOptions[option]?.description}</p>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
         <div className='unsubscribe-all-banner'>
           <div className='unsubscribe-all-banner-text'>
