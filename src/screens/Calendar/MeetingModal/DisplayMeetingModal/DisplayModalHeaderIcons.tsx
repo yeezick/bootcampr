e2f7a183955085d4e178104dd636bcde329a5564
Jordan Jennings <jordanjennings12@gmail.com>
2024-01-09 17:49:08 -0500

    create popover modal in display meeting modal

--- src/screens/Calendar/MeetingModal/DisplayMeetingModal/DisplayModalHeaderIcons.tsx
+++ src/screens/Calendar/MeetingModal/DisplayMeetingModal/DisplayModalHeaderIcons.tsx
@@ -1,16 +1,40 @@
+import * as React from 'react'
 import { useAppDispatch } from 'utils/redux/hooks'
 import { setModalDisplayStatus } from 'utils/redux/slices/calendarSlice'
-import { Close, EditNote } from '@mui/icons-material'
+import { Close } from '@mui/icons-material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { DisplayPopover } from './DisplayPopover'
 
 export const DisplayModalHeaderIcons = ({ handleClose, setDisplayMeeting }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClosePopover = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'simple-popover' : undefined
+
   const dispatch = useAppDispatch()
+
   const handleEdit = () => {
     dispatch(setModalDisplayStatus('edit'))
     setDisplayMeeting(false)
   }
   return (
     <div className='modal-icons'>
-      <EditNote onClick={handleEdit} />
+      <MoreVertIcon onClick={handleClick} />
+      <DisplayPopover
+        handleClosePopover={handleClosePopover}
+        handleEdit={handleEdit}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+      />
       <Close onClick={handleClose} />
     </div>
   )
---
+++ src/screens/Calendar/MeetingModal/DisplayMeetingModal/DisplayPopover.tsx
@@ -0,0 +1,40 @@
+import Popover from '@mui/material/Popover'
+import Typography from '@mui/material/Typography'
+
+export const DisplayPopover = ({
+  handleClosePopover,
+  handleEdit,
+  id,
+  open,
+  anchorEl,
+}) => {
+  return (
+    <>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClosePopover}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        sx={popoverStyles}
+      >
+        <Typography sx={{ p: 2 }} onClick={handleEdit}>
+          Edit
+        </Typography>
+        <Typography sx={{ p: 2 }}>Cancel Meeting</Typography>
+      </Popover>
+    </>
+  )
+}
+
+const popoverStyles = {
+  '& .MuiTypography-root': {
+    '&:hover': {
+      backgroundColor: '#00000014',
+      cursor: 'pointer',
+    },
+  },
+}
