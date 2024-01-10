90d840bc99d81945ab3ed91b7085ef8d78230af5
GeoSauer <geoffrey.sauer89@gmail.com>
2024-01-10 11:31:02 -0700

    updates text to match HiFi

--- src/screens/ProjectCompletion/PresentationPage.tsx
+++ src/screens/ProjectCompletion/PresentationPage.tsx
@@ -126,40 +126,76 @@ export const PresentationPage = ({ handlePageNavigation }) => {
             </div>
             <div className='projectcompletion__pre-para-content'>
               <p>
-                Now that your team has deployed the project, take this
-                opportunity to present in front of designers and developers and
-                get feedback on your work!
+                Present your team’s work to professional Product Managers, UX
+                Designers, and Software Engineers.
               </p>
             </div>
           </div>
           <div className='projectcompletion__pre-part-container'>
+            <div>
+              <h2>Why present your project?</h2>
+              <ul>
+                <li>
+                  Hone your presentation skills in front of the Bootcampr
+                  product team.
+                </li>
+                <li>
+                  Get feedback from people with experience launching a new
+                  product.
+                </li>
+                <li>Identify room for improvements.</li>
+                <li>
+                  Use the feedback you receive to present your work in
+                  interviews with confidence!
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2>Who is presenting your project?</h2>
+              <ul>
+                <li>
+                  At least one person must participate to present team’s work.
+                </li>
+                <li>
+                  Participation by all team members is not required but
+                  encouraged.
+                </li>
+              </ul>
+            </div>
             <div className='projectcompletion__pre-part-title'>
-              <h3>Let us know if you’d like to participate!</h3>
+              <h2>Let us know if your team will be presenting.</h2>
             </div>
             <div className='projectcompletion__pre-part-radios'>
-              <label className='projectcompletion__pre-part-label1' htmlFor=''>
+              <label
+                className='projectcompletion__pre-part-label1'
+                htmlFor='participate'
+              >
                 <input
                   type='radio'
+                  id='participate'
                   value='option1'
                   checked={selectedRadio === 'option1'}
                   onChange={handleRadioChange}
                 />
-                <p>Participate</p>
+                <p>My team will participate</p>
               </label>
-              <label className='projectcompletion__pre-part-label2' htmlFor=''>
+              <label
+                className='projectcompletion__pre-part-label2'
+                htmlFor='dontParticipate'
+              >
                 <input
                   type='radio'
+                  id='dontParticipate'
                   value='option2'
                   checked={selectedRadio === 'option2'}
                   onChange={handleRadioChange}
                 />
                 <div>
                   <p className='projectcompletion__pre-part-para1'>
-                    Don't participate
+                    My team will not participate
                   </p>
                   <p className='projectcompletion__pre-part-para2'>
-                    * your team can still decide to participate x day before Sep
-                    1
+                    *Please let us know by xx/xx if you plan to participate. 1
                   </p>
                 </div>
               </label>
