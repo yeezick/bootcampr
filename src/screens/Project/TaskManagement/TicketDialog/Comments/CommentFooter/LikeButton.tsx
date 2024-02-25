1f8ea652635fb81fc941389bcb08c60db0d5cbbe
Erick Manrique <ramerick5@gmail.com>
2024-02-25 00:39:54 -0600

    add and style like button tooltip showing liker names

--- src/screens/Project/TaskManagement/TicketDialog/Comments/CommentFooter/LikeButton.tsx
+++ src/screens/Project/TaskManagement/TicketDialog/Comments/CommentFooter/LikeButton.tsx
@@ -1,7 +1,9 @@
+import { Tooltip, TooltipProps, styled, tooltipClasses } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { updateComment } from 'utils/api/tickets'
 import { determineLikeIcon } from 'utils/helpers/commentHelpers'
 import { useAppSelector } from 'utils/redux/hooks'
+import { selectMembersById } from 'utils/redux/slices/projectSlice'
 import { selectUserId } from 'utils/redux/slices/userSlice'
 
 export const LikeButton = ({
@@ -11,6 +13,7 @@ export const LikeButton = ({
   toggleFetchComments,
 }) => {
   const [likedByUser, toggleLikedByUser] = useState(false)
+  const likers = useAppSelector(selectMembersById(likes))
   const userId = useAppSelector(selectUserId)
 
   useEffect(() => {
@@ -37,10 +40,42 @@ export const LikeButton = ({
     toggleFetchComments(!fetchComments)
   }
 
+  const tooltipTitle = likers
+    .map(liker => `${liker.firstName} ${liker.lastName}`)
+    .join(', ')
+
   return (
-    <div className='like-button'>
-      <div onClick={handleCommentLike}>{determineLikeIcon(likedByUser)}</div>
-      <p>{likes.length}</p>
-    </div>
+    <LikeToolTip
+      title={tooltipTitle}
+      placement='top-start'
+      slotProps={{
+        popper: {
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, -14],
+              },
+            },
+          ],
+        },
+      }}
+    >
+      <div className='like-button'>
+        <div onClick={handleCommentLike}>{determineLikeIcon(likedByUser)}</div>
+        <p>{likes.length}</p>
+      </div>
+    </LikeToolTip>
   )
 }
+
+const LikeToolTip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#424242',
+    fontSize: '12px',
+    lineHeight: '16px',
+    padding: '8px',
+  },
+}))
