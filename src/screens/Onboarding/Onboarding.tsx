0af24aaf5c2300906f909a82f9166a0a6cfa4173
Erick Manrique <ramerick5@gmail.com>
2024-03-05 19:16:38 -0600

    activate proejct portal on sign in add condition in onboarding to route user to project details if they have one change whats next url proejct id to unassigned

--- src/screens/Onboarding/Onboarding.tsx
+++ src/screens/Onboarding/Onboarding.tsx
@@ -20,7 +20,12 @@ export const Onboarding = () => {
     const hasSetAvailability = checkIfSetAvailability()
     const hasSetProfileInfo = checkIfSetProfileInfo()
 
-    if (authUser.role && hasSetAvailability && hasSetProfileInfo) {
+    if (
+      authUser.role &&
+      authUser.project &&
+      hasSetAvailability &&
+      hasSetProfileInfo
+    ) {
       navigate(`/project/${authUser.project}`)
     } else if (authUser.role && hasSetAvailability) {
       navigate(`/onboarding/${authUser._id}?pageId=setUpProfile`)
