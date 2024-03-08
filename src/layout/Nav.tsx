ff23c79d98dd59884c7571c5c0a1092ff30a72b7
ozge <ozgecaylioglu@gmail.com>
2024-03-08 16:29:02 -0500

    removed unnecessary useEffect

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -14,7 +14,6 @@ import {
   selectUnreadMessageCount,
   setActiveChatRoomId,
   toggleChat,
-  toggleChatClose,
 } from 'utils/redux/slices/chatSlice'
 
 import { AccountDropdown } from 'components/AccountDropdown.tsx/AccountDropdown'
@@ -39,14 +38,6 @@ export const Nav = () => {
   )
 
   const closeDropdown = () => setAnchorEl(null)
-
-  useEffect(() => {
-    // Close chat dialog and sideMenu when URL path changes
-    //TODO - close chat when click outside of chat box
-    // dispatch(toggleChatClose())
-    dispatch(setActiveChatRoomId(null))
-  }, [dispatch, location])
-
   const handlePortalLink = () => buildPortal(dispatch, 'project', projectId)
   const handleNonPortalLink = () => dispatch(resetPortal())
 
@@ -112,6 +103,7 @@ const AuthorizedNavLinks = ({ notificationCount, setAnchorEl }) => {
   useEffect(() => {
     //Warning: needs to be checked if members are loaded
     dispatch(fetchThreads())
+    //TODO - investigate why we set chatroom id to null :(
     dispatch(setActiveChatRoomId(null))
   }, [dispatch, projectMembers.length])
 
