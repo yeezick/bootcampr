f9c72e9278e9db8627c1c21090924535b354bfe2
ozge <ozgecaylioglu@gmail.com>
2024-03-08 10:50:48 -0500

    created auth layer on the top of all routes and protected some user routes

--- src/App.tsx
+++ src/App.tsx
@@ -27,57 +27,63 @@ import { WhatsNext } from 'screens/Onboarding/WhatsNext'
 import { Account, Email } from 'screens/Auth/Settings'
 import { SuccessScreen } from 'screens/SuccessScreen/SuccessScreen'
 import './App.css'
+import { AuthWrapper } from 'screens/Auth/AuthWrapper'
 
 function App() {
   console.log(`Running Application Version: ${process.env.REACT_APP_VERSION} `)
 
   return (
     <>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/about-us' element={<AboutUs />} />
-          <Route path='/how-to' element={<HowTo />} />
-          {/* Auth */}
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route
-            path='/sign-up/:id/confirmation-email-sent'
-            element={<EmailSentConfirmation />}
-          />
-          <Route path='/success/:userId' element={<SuccessScreen />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/users/:id/expired-link' element={<ExpiredLink />} />
-          <Route path='/users/:id/verify/:token' element={<EmailVerify />} />
-          <Route
-            path='/users/:id/reset-password/:token'
-            element={<ResetPassword />}
-          />
-          {/* User */}
-          <Route path='/onboarding/:id' element={<Onboarding />} />
-          <Route path='/whats-next' element={<WhatsNext />} />
-          <Route path='/users/:userId' element={<UserProfile />} />
-          <Route path='/users/:id/edit' element={<EditProfile />} />
-          <Route
-            path='/users/:id/update-email-confirmation'
-            element={<UpdateEmailConfirmation />}
-          />
-          <Route path='/users/:id/settings'>
-            <Route path='email' element={<Email />} />
-            <Route path='password' element={<ChangePassword />} />
-            <Route path='account' element={<Account />} />
-          </Route>
-          {/* Project */}
-          <Route path='/project'>
-            <Route path=':projectId' element={<ProjectDetails />} />
-            <Route path=':projectId/complete' element={<ProjectCompletion />} />
-            <Route path=':projectId/calendar' element={<CalendarScreen />} />
-            <Route path=':projectId/tasks' element={<TaskManagement />} />
-            <Route path=':projectId/team' element={<TeamMembers />} />
-          </Route>
-          <Route path='/all-projects' element={<AllProjects />} />
-        </Routes>
-      </Layout>
-      <SnackBarToast />
+      <AuthWrapper>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/how-to' element={<HowTo />} />
+            {/* Auth */}
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route
+              path='/sign-up/:id/confirmation-email-sent'
+              element={<EmailSentConfirmation />}
+            />
+            <Route path='/success/:userId' element={<SuccessScreen />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/users/:id/expired-link' element={<ExpiredLink />} />
+            <Route path='/users/:id/verify/:token' element={<EmailVerify />} />
+            <Route
+              path='/users/:id/reset-password/:token'
+              element={<ResetPassword />}
+            />
+            {/* User */}
+            <Route path='/onboarding/:id' element={<Onboarding />} />
+            <Route path='/whats-next' element={<WhatsNext />} />
+            <Route path='/users/:userId' element={<UserProfile />} />
+            <Route path='/users/:id/edit' element={<EditProfile />} />
+            <Route
+              path='/users/:id/update-email-confirmation'
+              element={<UpdateEmailConfirmation />}
+            />
+            <Route path='/users/:id/settings'>
+              <Route path='email' element={<Email />} />
+              <Route path='password' element={<ChangePassword />} />
+              <Route path='account' element={<Account />} />
+            </Route>
+            {/* Project */}
+            <Route path='/project'>
+              <Route path=':projectId' element={<ProjectDetails />} />
+              <Route
+                path=':projectId/complete'
+                element={<ProjectCompletion />}
+              />
+              <Route path=':projectId/calendar' element={<CalendarScreen />} />
+              <Route path=':projectId/tasks' element={<TaskManagement />} />
+              <Route path=':projectId/team' element={<TeamMembers />} />
+            </Route>
+            <Route path='/all-projects' element={<AllProjects />} />
+          </Routes>
+        </Layout>
+        <SnackBarToast />
+      </AuthWrapper>
     </>
   )
 }
---
+++ src/screens/Auth/AuthWrapper.tsx
@@ -0,0 +1,55 @@
+import { Loader } from 'components/Loader/Loader'
+import { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { verify } from 'utils/api/users'
+import { storeUserProject } from 'utils/helpers/stateHelpers'
+import { useAppDispatch, useAppSelector } from 'utils/redux/hooks'
+import {
+  selectAuthUser,
+  uiStatus,
+  updateAuthUser,
+} from 'utils/redux/slices/userSlice'
+
+const unprotectedRoutes = ['/', '/sign-in', '/sign-up', '/how-to', '/about-us']
+
+export const AuthWrapper = ({ children }) => {
+  const dispatch = useAppDispatch()
+  const location = useLocation()
+  const navigate = useNavigate()
+  const authUser = useAppSelector(selectAuthUser)
+  const status = useAppSelector(uiStatus)
+
+  useEffect(() => {
+    const verifyAndNavigateUser = async () => {
+      try {
+        const token = localStorage.getItem('bootcamprAuthToken')
+        //to syncronize and get user info again after refreshing the page
+        if (token && !authUser?._id) {
+          const verifiedAuthUser = await verify()
+          if (verifiedAuthUser?._id) {
+            await storeUserProject(dispatch, verifiedAuthUser.project)
+            dispatch(updateAuthUser(verifiedAuthUser))
+          } else if (!unprotectedRoutes.includes(location.pathname)) {
+            navigate('/sign-in')
+          }
+        } else if (!token && !unprotectedRoutes.includes(location.pathname)) {
+          //user is not verified and the route is protected
+          navigate('/sign-in')
+        }
+      } catch (error) {
+        console.error('Error in auth verification: ', error)
+        if (!unprotectedRoutes.includes(location.pathname)) {
+          navigate('/sign-in')
+        }
+      }
+    }
+
+    verifyAndNavigateUser()
+  }, [authUser._id, dispatch, location.pathname, navigate])
+
+  if (status.isLoading) {
+    return <Loader />
+  }
+
+  return <>{children}</>
+}
