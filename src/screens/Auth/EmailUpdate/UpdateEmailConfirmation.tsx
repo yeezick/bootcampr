bcd07ac26641d50fe19e7d36b33e9d9e2edb74a2
GeoSauer <geoffrey.sauer89@gmail.com>
2024-04-17 12:57:14 -0600

    Update styling

--- src/screens/Auth/EmailUpdate/UpdateEmailConfirmation.tsx
+++ src/screens/Auth/EmailUpdate/UpdateEmailConfirmation.tsx
@@ -10,18 +10,22 @@ export const UpdateEmailConfirmation = () => {
   const [email, setEmail] = useState(newEmail)
 
   return (
-    <div className='update-email-confirmation'>
-      <div className='content'>
-        <h1>You've updated your email address!</h1>
-        <h2>
-          We sent a confirmation email to <span>{email}</span>.
-        </h2>
+    <div className='email-confirmation-container'>
+      <p className='message-content header'>
+        You've updated your email address!
+      </p>
+      <p className='message-content subheader'>
+        We sent a confirmation email to <span>{email}</span>.
+      </p>
+      <div className='message-content text'>
         <p>It may be in your junk or spam folder.</p>
-        <h3>Confirm your email address to log in.</h3>
-        <div className='message-content update-email'>
-          <UpdateEmailAddressLink setEmail={setEmail} />
-          <span> if it's incorrect.</span>
-        </div>
+      </div>
+      <div className='message-content confirm'>
+        <p>Confirm your email address to log in.</p>
+      </div>
+      <div className='message-content update-email'>
+        <UpdateEmailAddressLink setEmail={setEmail} />
+        <span> if it's incorrect.</span>
       </div>
     </div>
   )
