66a4d6ab0baa4cc1237b1b2bc30bc071b0cad00f
GeoSauer <geoffrey.sauer89@gmail.com>
2024-05-22 16:30:15 -0600

    Refactor to use MUI comps, begin styling changes

--- src/screens/Project/ProjectDetails/ProjectDetails.tsx
+++ src/screens/Project/ProjectDetails/ProjectDetails.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Overview } from './Overview'
 import { ProjectTimeline } from './ProjectTimeline'
 import { Presentation } from './Presentation'
 import './ProjectDetails.scss'
+import { Box, Tab } from '@mui/material'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
 
 export const ProjectDetails = () => {
   const tabData = [
@@ -11,69 +13,72 @@ export const ProjectDetails = () => {
     { label: 'PRESENTATION', content: <Presentation /> },
   ]
 
-  return (
-    <div className='project-details-portal'>
-      <div className='pd-cont'>
-        <div className='pd-header-cont'>
-          <RenderTab tabs={tabData} />
-        </div>
-      </div>
-    </div>
-  )
-}
+  const [value, setValue] = useState('1')
 
-const RenderTab = ({ tabs }) => {
-  const [activeTab, setActiveTab] = useState(0)
-  const [indicatorWidth, setIndicatorWidth] = useState(0)
-  const [indicatorLeft, setIndicatorLeft] = useState(0)
-
-  const handleTabClick = (tabIndex: number) => {
-    setActiveTab(tabIndex)
-    const buttonWidth = document.getElementById(`tab-${tabIndex}`)?.offsetWidth
-    const buttonLeft = document.getElementById(`tab-${tabIndex}`)?.offsetLeft
-
-    setIndicatorWidth(buttonWidth)
-    setIndicatorLeft(buttonLeft)
-  }
-
-  useEffect(() => {
-    const buttonWidth = document.getElementById(`tab-${activeTab}`)?.offsetWidth
-    setIndicatorWidth(buttonWidth)
-  }, [activeTab])
-
-  return (
-    <div className='pd-nav-cont'>
-      <div className='pd-nav-tabs-btns'>
-        {tabs.map((tab, index) => (
-          <TabButton
-            key={index}
-            id={`tab-${index}`}
-            tabIndex={index}
-            handleTabClick={handleTabClick}
-            tabName={tab.label}
-          />
-        ))}
-        <span
-          className='pd-nav-indicator'
-          style={{
-            width: `${indicatorWidth}px`,
-            transform: `translateX(${indicatorLeft}px)`,
-          }}
-        />
-      </div>
-      <div className='pd-nav-content-cont'>{tabs[activeTab].content}</div>
-    </div>
-  )
-}
-
-const TabButton = ({ id, tabIndex, handleTabClick, tabName }) => {
-  const handleClick = () => {
-    handleTabClick(tabIndex)
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue)
   }
 
   return (
-    <button id={id} className='pd-nav-tabs' onClick={handleClick}>
-      <h1>{tabName}</h1>
-    </button>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box>
+          <TabList
+            sx={{ ...tabListStyles }}
+            onChange={handleChange}
+            aria-label='Product Details navigation'
+          >
+            {/* {tabData.map((tab, idx) => (
+              <CustomTab label={tab.label} value={`${idx} + 1`} />
+            ))} */}
+            <Tab label='PROJECT BRIEF' value='1' sx={tabStyles} {...tabProps} />
+            <Tab label='TIMELINE' value='2' sx={tabStyles} {...tabProps} />
+            <Tab label='PRESENTATION' value='3' sx={tabStyles} {...tabProps} />
+          </TabList>
+        </Box>
+        <TabPanel value='1' sx={{ marginLeft: '10px' }}>
+          <Overview />
+        </TabPanel>
+        <TabPanel value='2' sx={{ marginLeft: '10px' }}>
+          <ProjectTimeline />
+        </TabPanel>
+        <TabPanel value='3' sx={{ marginLeft: '10px' }}>
+          <Presentation />
+        </TabPanel>
+      </TabContext>
+    </Box>
   )
+}
+
+// const CustomTab = ({ label, value }) => {
+//   return <Tab label={label} value={value} />
+// }
+const tabListStyles = {
+  background: '#fff',
+  marginLeft: '32px',
+  marginBottom: '-40px',
+  marginTop: '16px',
+  paddingBottom: '16px',
+  '& .MuiTabs-indicator': {
+    backgroundColor: '#ffa726',
+    height: '4px',
+  },
+  '& .MuiTab-root': {
+    minWidth: 'auto',
+    marginRight: '16px',
+    color: 'black',
+    '&.Mui-selected': {
+      color: 'black',
+    },
+  },
+}
+const tabStyles = {
+  border: '1px solid violet',
+  padding: '0',
+  fontSize: '16px',
+  fontWeight: '600',
+}
+const tabProps = {
+  disableRipple: true,
+  disableFocusRipple: true,
 }
