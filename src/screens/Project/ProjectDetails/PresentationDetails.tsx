55b25f7f698ddee2ab0a18cd976aecc9f998d5df
ozge <ozgecaylioglu@gmail.com>
2024-06-17 22:36:54 -0400

    updated presentation and completed project info

--- src/screens/Project/ProjectDetails/PresentationDetails.tsx
+++ src/screens/Project/ProjectDetails/PresentationDetails.tsx
@@ -4,21 +4,29 @@ import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined'
 import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined'
 import { useAppSelector } from 'utils/redux/hooks'
 import { getUserTimezone } from 'utils/redux/slices/userSlice'
-import { selectPresentationDate } from 'utils/redux/slices/projectSlice'
+import {
+  selectPresentationDate,
+  selectPresentationDateWithTime,
+  selectProjectId,
+} from 'utils/redux/slices/projectSlice'
 import {
   formatPresentationDate,
   convertPresentationDateUserTZ,
 } from 'utils/helpers/projectHelpers'
 import './PresentationDetails.scss'
 import { convertOffsetToTimezone } from 'utils/data/timeZoneConstants'
+import { isSandboxId } from 'utils/helpers/taskHelpers'
 
 export const PresentationDetails = () => {
+  const currentProjectId = useAppSelector(selectProjectId)
   const userTimezoneOffset = useAppSelector(getUserTimezone)
-  const presentationDateEST = useAppSelector(selectPresentationDate)
-  const isPresentationDateExist = presentationDateEST.startDateEST.isValid()
+  const presentationDateEST = useAppSelector(selectPresentationDateWithTime)
+  const presentationInfo = useAppSelector(selectPresentationDate)
   let presentationDate
 
-  if (isPresentationDateExist) {
+  if (!presentationInfo || isSandboxId(currentProjectId)) {
+    presentationDate = 'To be determined'
+  } else {
     const userTimezoneInfo = convertOffsetToTimezone[userTimezoneOffset]
     const { startDate, endDate } = convertPresentationDateUserTZ(
       presentationDateEST,
@@ -29,8 +37,6 @@ export const PresentationDetails = () => {
       endDate,
       userTimezoneInfo.abbr
     )
-  } else {
-    presentationDate = 'To be determined'
   }
 
   return (
