c96c815faa440cc1daccad901b493e3872c99175
ozge <ozgecaylioglu@gmail.com>
2024-06-19 23:37:23 -0400

    removed unused code

--- src/utils/data/projectConstants.ts
+++ src/utils/data/projectConstants.ts
@@ -40,22 +40,6 @@ export const emptyProject: ProjectInterface = {
   completed: false,
 }
 
-// export const emptyProject = {
-//   duration: '',
-//   meeting_cadence: '',
-//   overview: '',
-//   project_owner: '',
-//   status: '',
-//   technologies_used: [],
-//   title: '',
-// }
-
-// export const emptyProjectOwner = {
-//   firstName: '',
-//   lastName: '',
-//   _id: '',
-// }
-
 export enum ProjectIcons {
   Unassigned = 'https://tinyurl.com/4wbe9hp2',
 }
