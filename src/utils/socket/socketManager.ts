77ca0663d32a8c30640a4f353624811e6e592eb4
ozge <ozgecaylioglu@gmail.com>
2024-07-10 15:47:00 -0400

    created singleton socket manager and hook

---
+++ src/utils/socket/socketManager.ts
@@ -0,0 +1,44 @@
+import io from 'socket.io-client'
+import { api } from 'utils/api'
+
+const ENDPOINT = api.getUri()
+const sockets = {}
+
+export const connectSocket = (namespace, userId) => {
+  if (!sockets[namespace]) {
+    const socket = io(`${ENDPOINT}/${namespace}`, {
+      query: { userId },
+      transports: ['websocket'],
+      reconnection: true,
+      reconnectionAttempts: 5,
+      reconnectionDelay: 3000,
+    })
+    socket.on('connect', () => {
+      socket.emit('setUserId', userId)
+    })
+    socket.on('disconnect', () => {
+      delete sockets[namespace]
+    })
+
+    sockets[namespace] = socket
+    return socket
+  }
+  return sockets[namespace]
+}
+
+export const disconnectSocket = namespace => {
+  const socket = sockets[namespace]
+  if (socket) {
+    socket.disconnect()
+    delete sockets[namespace]
+  }
+}
+
+export const emitEvent = (namespace, event, data) => {
+  const socket = sockets[namespace]
+  if (socket) {
+    socket.emit(event, data)
+  } else {
+    console.error(`There is no socket connection for namespace: ${namespace}`)
+  }
+}
